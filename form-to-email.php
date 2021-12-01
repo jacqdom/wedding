81db18e6be2da9e9a3fe3d915cda5ba6f437777b
jacqdom <95316749+jacqdom@users.noreply.github.com>
2021-12-01 18:22:15 -0500

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -16,9 +16,42 @@
 
   $headers = "Reply-To: $visitor_email \r\n";
 
-  mail($to,$email_subject,$email_body,$headers);
+//mail($to,$email_subject,$email_body,$headers);
  echo "mailed to";
  echo $to;
+
+//php mail handler through heroku trustifi
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"rmota29619@gmail.com\"}],\"title\":\"Title\",\"html\":\"Body\"}",
+    CURLOPT_HTTPHEADER => array(
+        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
+        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
+        "content-type: application/json"
+    )
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
+//end of heroku handler
+
+
+
   function IsInjected($str)
   {
     $injections = array('(\n+)',
