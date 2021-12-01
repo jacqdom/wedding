5f4f993bc6c8b06c62e03a2130f41f1c67f787b5
jacqdom <95316749+jacqdom@users.noreply.github.com>
2021-12-01 18:37:24 -0500

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -14,11 +14,9 @@
 
   $headers = "From: $email_from \r\n";
 
-  $headers = "Reply-To: $visitor_email \r\n";
+  $headers .= "Reply-To: $visitor_email \r\n";
 
 //mail($to,$email_subject,$email_body,$headers);
- echo "mailed to";
- echo $to;
 
 //php mail handler through heroku trustifi
 
