b83a6bd14eb3cdfc9ca9a58653bbf8f7c64acf3a
jacqdom <95316749+jacqdom@users.noreply.github.com>
2021-12-01 20:20:57 -0500

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,55 +1,13 @@
 <?php
- echo "beginning";
   $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
-
-	 $email_from = 'rmota29619@gmail.com';
-
- 	$email_subject = "New Form submission";
-
- 	$email_body = "You have received a new message from the user $name.\n";
-    
+  $rsvp = $_POST['rsvp'];
+  $dinner = $_POST['dinner'];
+  $email_from = 'rmota29619@gmail.com';
+  $email_subject = "Wedding Reservation";
+  $email_body = "You have received an rsvp from $name.\n";
   $to = "rmota29619@gmail.com";
 
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-//mail($to,$email_subject,$email_body,$headers);
-
-//php mail handler through heroku trustifi
-
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"rmota29619@gmail.com\"}],\"title\":\"Title\",\"html\":\"Body\"}",
-    CURLOPT_HTTPHEADER => array(
-        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
-        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
-        "content-type: application/json"
-    )
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    echo $response;
-}
-//end of heroku handler
-
-
-
+//Injection prevention handler
   function IsInjected($str)
   {
     $injections = array('(\n+)',
@@ -74,10 +32,40 @@ if ($err) {
     }
 }
 
-if(IsInjected($visitor_email))
+if(IsInjected($name))
 {
-    echo "Bad email value!";
+    echo "Bad name value!";
     exit;
 }
+//Injection prevention handler
+
+//php mail handler through heroku trustifi
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"rmota29619@gmail.com\"}],\"title\":\"Title\",\"html\":\"Hello. $email_body RSVP: $rsvp .\n Meal Choice: $dinner\"}",
+    CURLOPT_HTTPHEADER => array(
+        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
+        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
+        "content-type: application/json"
+    )
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo "Success! Thankyou!";
+}
+//end of heroku handler
 
 ?>
